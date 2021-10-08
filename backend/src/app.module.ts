bbeb284d50265528a90e5554d3a5504d0f4e8459
Vinit Tomar <32155118+VinitTomar@users.noreply.github.com>
2021-10-08 16:45:53 +0530

    env.DB_HOST renamed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,7 +21,7 @@ import { ConfigModule } from '@nestjs/config';
     LoginModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOS || 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       port: +process.env.DB_PORT || 3306,
       username: process.env.DB_PASSWORD || 'root',
       password: process.env.DB_USERNAME || 'root',
