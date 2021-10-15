8a64f6a4eb6b1e00e700f701770455f9cf466563
Vinit Tomar <vinit.tomar68@gmail.com>
2021-10-15 16:38:37 +0530

    unit testing auth module

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,42 @@
+import { User } from './../users/user.entity';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UsersService;
+  const dummyUser: User = {
+    id: 1,
+    username: 'user',
+    password: 'pass'
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            findOne: () => { }
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return valid user', async () => {
+    jest.spyOn(userService, 'findOne').mockImplementation(async () => dummyUser);
+    expect(await service.validateUser('usr', 'pass')).toBeDefined();
+    expect(userService.findOne).toHaveBeenCalled();
   });
 });
--- backend/src/auth/token.service.spec.ts
+++ backend/src/auth/token.service.spec.ts
@@ -1,12 +1,26 @@
+import { JwtPayload } from './jwt-payload';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenService } from './token.service';
 
 describe('TokenService', () => {
   let service: TokenService;
+  const dummyJwt = 'dummy_jwt';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TokenService],
+      providers: [
+        TokenService,
+        {
+          provide: JwtService,
+          useValue: {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            sign: (_: JwtPayload) => {
+              return dummyJwt;
+            }
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<TokenService>(TokenService);
@@ -14,5 +28,9 @@ describe('TokenService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be give jwt', () => {
+    expect(service.getJwtToken({sub: 'sub'})).toEqual(dummyJwt);
   });
 });
