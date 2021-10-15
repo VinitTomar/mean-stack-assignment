1a4087c1833dd0ed6a0937086205bac095c10a2a
Vinit Tomar <vinit.tomar68@gmail.com>
2021-10-15 14:18:56 +0530

    unit testing product service

--- backend/src/product/product.entity.ts
+++ backend/src/product/product.entity.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty, Min } from 'class-validator';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
 
 @Entity()
--- backend/src/product/product.service.spec.ts
+++ backend/src/product/product.service.spec.ts
@@ -1,12 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Product } from './product.entity';
+import { DeleteResult, Repository } from 'typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+type MockType<T> = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [P in keyof T]?: jest.Mock<{}>;
+};
 
 describe('ProductService', () => {
   let service: ProductService;
+  const dummyProducts: Product[] = [
+    {
+      id: '1',
+      name: 'Prod one',
+      description: 'Prod one detail',
+      price: 100,
+      user: null,
+      createdAt: new Date()
+    },
+    {
+      id: '2',
+      name: 'Prod two',
+      description: 'Prod two detail',
+      price: 200,
+      user: null,
+      createdAt: new Date()
+    },
+  ];
+
+
+  const productRepositoryMockFactory :() => MockType<Repository<Product>> = jest.fn(() => ({
+    save: jest.fn(entity => {
+      return {
+        id: '1', ...entity
+      }
+    }),
+
+    find: jest.fn(_ => {
+      return dummyProducts;
+    }),
+
+    delete: jest.fn(_ => {
+      return {
+        raw: 'deleted'
+      };
+    })
+  }));
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: getRepositoryToken(Product),
+          useFactory: productRepositoryMockFactory
+        }
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
@@ -14,5 +66,20 @@ describe('ProductService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should add product', async () => {
+    expect(await service.addProduct(dummyProducts[0])).toEqual({
+      id: '1',
+      ...dummyProducts[0]
+    });
+  });
+
+  it('should find all products by user id', async () => {
+    expect(await service.findAllByUserId(1)).toEqual(dummyProducts);
+  });
+
+  it('should delete product', async () => {
+    expect(await service.removeProduct('1', 1)).toBeTruthy();
   });
 });
